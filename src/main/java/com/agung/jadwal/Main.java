5d80f7a7608dc2dc345dc95cf4163d7bb06366f6
agungpramono <agungpermadi13@gmail.com>
2016-12-03 11:06:53 +0700

    hapus method tidak dipakai

--- src/main/java/com/agung/jadwal/Main.java
+++ src/main/java/com/agung/jadwal/Main.java
@@ -9,15 +9,9 @@
 package com.agung.jadwal;
 
 import com.agung.jadwal.dao.InstructorDao;
-import com.agung.jadwal.dao.JadwalDao;
-import com.agung.jadwal.dao.KoneksiHelper;
-import com.agung.jadwal.dao.RuangDao;
 import com.agung.jadwal.domain.Trainer;
 
 import com.agung.jadwal.ui.MainFrame;
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
-
-import java.sql.SQLException;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -27,19 +21,7 @@ import javax.swing.UnsupportedLookAndFeelException;
 /**
  *
  */
-public class Main {
-    
-    private static final ch.qos.logback.classic.Logger LOGGER = new ch.qos.logback.classic.LoggerContext()
-            .getLogger(Main.class);
-    private static InstructorDao instructorDao = new InstructorDao();
-    
-    public static void test(){
-        List<Trainer> resultAll = instructorDao.cariSemua();
-        for (Trainer t : resultAll) {
-            System.out.println("kode "+ t.getKodeGuru());
-        }
-    }
-    
+public class Main {  
     public static void setLookAndFeel(){
         for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Metal".equals(info.getName())) {
@@ -57,8 +39,6 @@ public class Main {
     }
    
     public static void main(String[] args) throws Exception{
-        test();
-        LOGGER.debug("----- Memulai Aplikasi -----");
         setLookAndFeel();
         SwingUtilities.invokeLater(new Runnable() {
             @Override
@@ -66,7 +46,5 @@ public class Main {
                 new MainFrame().setVisible(true);
             }
         });
-        
-        LOGGER.debug("--- Mengakhiri Aplikasi");
     }
 }
